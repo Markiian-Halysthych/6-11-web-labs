682252002ed088d9defee119b0c85c135ce14f07
Markiian Halysthych <markijan8890.mg@gmail.com>
2021-02-12 20:37:15 +0200

    add 6-11 labs

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from "../accets/img/logo.png"
+import "../accets/css/components-style/footer.css"
+function Footer(props) {
+    return (
+        <footer className="footer">
+
+            <a className="header__title transition" href="/">
+                CAR SHOP
+
+            </a>
+            <p className="footer-text">2021 IOT  ©Copyright all rights reserved</p>
+
+
+        </footer>
+    );
+}
+
+export default Footer;
